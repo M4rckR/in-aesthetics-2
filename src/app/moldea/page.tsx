ac9380d3dd0a66f9ffc0590a04c749fb75ec8605
m4arck <marcromerogar4@gmail.com>
2025-05-02 10:45:27 -0500

    Proyecto iniciado

---
+++ src/app/moldea/page.tsx
@@ -0,0 +1,7 @@
+export default function Moldeador() {
+  return (
+    <div>
+      <h1>Moldeador</h1>
+    </div>
+  );
+}
---
+++ src/app/verruga/page.tsx
@@ -0,0 +1,7 @@
+export default function Verruga() {
+  return (
+    <div>
+      <h1>Verruga</h1>
+    </div>
+  );
+}
