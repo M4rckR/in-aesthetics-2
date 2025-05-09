dc58a44615668b0f719daa8b0224c584cd35ee7f
m4arck <marcromerogar4@gmail.com>
2025-05-09 16:52:46 -0500

    fix: hero image/lunares

--- src/app/lunares/components/HeroLaserTreatment.tsx
+++ src/app/lunares/components/HeroLaserTreatment.tsx
@@ -15,7 +15,7 @@ export const HeroLaserTreatment = () => {
         className="object-cover"
       />
       <div className="absolute inset-0 bg-gradient-to-t from-[#571E03] to-transparent opacity-80" />
-      <div className="bottom-8 md:top-auto md:translate-y-0 absolute text-center max-w-[1200px] lg:left-1/2 lg:-translate-x-1/2 flex flex-col items-center md:bottom-12 md:space-y-4 text-white px-6 lg:px-0">
+      <div className="bottom-8 md:top-auto md:translate-y-0 absolute text-center max-w-[1200px] lg:left-1/2 lg:-translate-x-1/2 flex flex-col items-center md:bottom-16 md:space-y-0 text-white px-6 lg:px-0">
         <p className="text-lg md:text-xl">Nuestro tratamiento con</p>
         <h2 className="text-center font-in-playfair text-[42px] lg:text-6xl pb-4">Láser CO2 DEKA</h2>
         <p className="text-sm md:text-base pb-6">Es la solución más avanzada para eliminar lunares, verrugas y acrocordones sin dolor ni marcas. Es un procedimiento rápido, seguro y eficaz, realizado por especialistas en estética avanzada.</p>
