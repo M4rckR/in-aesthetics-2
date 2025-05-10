581701938e74dea7250b4dcd392caefec3f6013d
m4arck <marcromerogar4@gmail.com>
2025-05-10 13:00:04 -0500

    Google Tag Manager Agregado

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,6 +8,7 @@ import 'aos/dist/aos.css'
 import { Footer } from "@/components/Footer";
 import { AOSInit } from "@/components/aosInit/aosInit";
 import { Toaster } from "sonner";
+import Script from "next/script";
 
 const playfair = Playfair_Display({
   variable: "--font-in-playfair",
@@ -35,10 +36,32 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" className="h-full scroll-smooth">
+      <head>
+        {/* Google Tag Manager - Script parte */}
+        <Script id="google-tag-manager" strategy="afterInteractive">
+          {`
+            (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P5VN8HXJ');
+          `}
+        </Script>
+      </head>
       <AOSInit />
       <body
         className={`${poppins.variable} ${playfair.variable} antialiased flex flex-col min-h-screen`}
-        >
+      >
+        {/* Google Tag Manager - noscript parte */}
+        <noscript>
+          <iframe 
+            src="https://www.googletagmanager.com/ns.html?id=GTM-P5VN8HXJ"
+            height="0" 
+            width="0" 
+            style={{ display: 'none', visibility: 'hidden' }}
+          />
+        </noscript>
+        
         <Toaster/>
         <main className="flex-grow">
           {children}
