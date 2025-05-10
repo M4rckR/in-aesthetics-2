b364eeddcaf3960f0d56db0d2b5d1dc44bdbc532
m4arck <marcromerogar4@gmail.com>
2025-05-10 09:22:17 -0500

    fix: delete imports

--- src/app/reduccion/components/testimonials/TestimonialCarousel.tsx
+++ src/app/reduccion/components/testimonials/TestimonialCarousel.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
+import { Avatar, AvatarFallback } from "@/components/ui/avatar";
 import Image from "next/image";
 import { Swiper, SwiperSlide } from "swiper/react";
 import { Autoplay, Pagination } from "swiper/modules";
