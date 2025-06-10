472dc80c60fa7d651a1b842ae88033aecc754a65
m4arck <marcromerogar4@gmail.com>
2025-06-10 13:55:41 -0500

    Cambio de imagenes

--- src/app/enzimas/components/HeroContact.tsx
+++ src/app/enzimas/components/HeroContact.tsx
@@ -13,6 +13,7 @@ export const HeroContact = () => {
                 priority
                 fill
                 className=" object-contain"
+                unoptimized
             />
           </div>
 
