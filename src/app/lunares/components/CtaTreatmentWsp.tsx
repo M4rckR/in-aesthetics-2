47c64427b44e717437769e18b25dc318912ac55a
m4arck <marcromerogar4@gmail.com>
2025-06-12 14:33:15 -0500

    Imagenes actualizadas

--- src/app/lunares/components/CtaTreatmentWsp.tsx
+++ src/app/lunares/components/CtaTreatmentWsp.tsx
@@ -14,7 +14,7 @@ export const CtaTreatmentWsp = () => {
                 </a>
             </div>
             <Image 
-                src={'/images/moldea/imageTreatment.png'} 
+                src='/images/moldea/imageTreatment.png'
                 alt='cta-treatment-wsp' 
                 width={100} 
                 height={100}
