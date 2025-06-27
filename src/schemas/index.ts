06617265fda2fade0f3970196b093c93a1242511
m4arck <marcromerogar4@gmail.com>
2025-06-27 16:44:06 -0500

    Types mejorados

--- src/schemas/index.ts
+++ src/schemas/index.ts
@@ -4,17 +4,10 @@ export const ClientSchema = z.object({
         .min(2, "Nombre inválido")
         .max(50, "Nombre demasiado largo")
         .regex(/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/, "Nombre inválido"),
-    // correo: z.string()
-    //     .email("Correo inválido")
-    //     .toLowerCase(),
     telefono: z.string()
         .min(9, "Teléfono muy corto")
         .max(9, "Teléfono muy largo")
-        .regex(/^\d+$/, "Teléfono inválido"),
-    // mensaje: z.string()
-    //     .min(5, "Mensaje demasiado corto")
-    //     .max(500, "Mensaje demasiado largo")
-    //     .optional(),
+        .regex(/^9\d{8}$/, "Teléfono inválido"),
     honeypot: z.string().max(0).optional(),
 })
 
