c5a6452173ec0b50c9ae77ec9ec10cfc325ea45b
M4rckR <marcromerogar4@gmail.com>
2025-07-25 11:36:37 -0500

    Updated: Eliminacion del GTM y GA4

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,9 +6,6 @@ import "@/styles/index.css";
 import 'aos/dist/aos.css'
 import { AOSInit } from "@/components/aosInit/aosInit";
 import { Toaster } from "sonner";
-import { GoogleTagManager } from '@next/third-parties/google';
-import { GoogleAnalytics } from '@next/third-parties/google';
-import { Analytics } from "@vercel/analytics/next"
 import localFont from 'next/font/local'
 
 const ProximaNova = localFont({
@@ -48,28 +45,14 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" suppressHydrationWarning  className="h-full scroll-smooth">
-      <GoogleTagManager gtmId="GTM-P5VN8HXJ" />
       <AOSInit />
       <body suppressHydrationWarning
         className={`${poppins.variable} ${playfair.variable} ${ProximaNova.variable} antialiased flex flex-col min-h-screen`}
       >
-        {/* <!-- Google Tag Manager (noscript) --> */}
-        <noscript>
-          <iframe 
-            src="https://www.googletagmanager.com/ns.html?id=GTM-P5VN8HXJ"
-            height="0" 
-            width="0" 
-            style={{display: 'none', visibility: 'hidden'}}
-          />
-        </noscript>
-        {/* <!-- End Google Tag Manager (noscript) --> */}
         <Toaster/>
         <main className="flex-grow">
           {children}
         </main>
-
-        <GoogleAnalytics gaId="G-6YF0S48H61" />
-        <Analytics />
       </body>
     </html>
   );
