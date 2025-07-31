2418d26174506c9ae8f8c2c4b3815bcf6f35b3e3
M4rckR <marcromerogar4@gmail.com>
2025-07-31 09:21:42 -0500

    Updated: "Analitica nueva GTM añadida"

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -8,7 +8,7 @@ import { AOSInit } from "@/components/aosInit/aosInit";
 import { Toaster } from "sonner";
 import localFont from 'next/font/local'
 import { ReactNode } from "react";
-import {GoogleTagManager} from "@next/third-parties/google";
+import Script from "next/script";
 
 const ProximaNova = localFont({
   src: '../../public/fonts/ele.otf',
@@ -51,12 +51,26 @@ export default function RootLayout({
       <body suppressHydrationWarning
         className={`${poppins.variable} ${playfair.variable} ${ProximaNova.variable} antialiased flex flex-col min-h-screen`}
       >
-
+      <Script id="google-tag-manager" strategy="afterInteractive">
+        {
+          `
+           (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+            new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+            j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+            'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+            })(window,document,'script','dataLayer','GTM-P5VN8HXJ');
+          `
+        }
+      </Script>
+      <noscript>
+        <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-P5VN8HXJ"
+                height="0" width="0" style={{ display: "none", visibility:"hidden"}}>
+        </iframe>
+      </noscript>
         <Toaster/>
         <main className="flex-grow">
           {children}
         </main>
-        <GoogleTagManager gtmId="GTM-P5VN8HXJ" />
       </body>
     </html>
   );
