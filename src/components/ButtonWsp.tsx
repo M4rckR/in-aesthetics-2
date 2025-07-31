ebf067053f60a288a794172ce9208a3eb2c148a2
M4rckR <marcromerogar4@gmail.com>
2025-07-31 11:34:38 -0500

    Updated: "Se cambio ID de lugar para precision"

--- src/components/ButtonWsp.tsx
+++ src/components/ButtonWsp.tsx
@@ -10,8 +10,8 @@ export const ButtonWsp = ({message, phoneNumber = "51974309260"}:ButtonWspProps)
    const encodedMessage = encodeURIComponent(message)
 
     return (
-      <div id={"floating-wsp-custom"} className="container max-w-[1200] mx-auto relative">
-          <a className="fixed bottom-1/12 w-12 h-12 right-6 z-50  rounded-full shadow-lg transition-transform duration-300 hover:scale-105 bg-in-brown flex justify-center items-center" href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`} target="_blank" rel="noopener noreferrer">
+      <div  className="container max-w-[1200] mx-auto relative">
+          <a id={"floating-wsp-custom"} className="fixed bottom-1/12 w-12 h-12 right-6 z-50  rounded-full shadow-lg transition-transform duration-300 hover:scale-105 bg-in-brown flex justify-center items-center" href={`https://api.whatsapp.com/send?phone=${phoneNumber}&text=${encodedMessage}`} target="_blank" rel="noopener noreferrer">
              <Image
                 alt="icono whatsapp"
                 src="/icons/button_wsp.svg"
@@ -21,6 +21,6 @@ export const ButtonWsp = ({message, phoneNumber = "51974309260"}:ButtonWspProps)
              />
           </a>
       </div>
-      
+
     )
   }
